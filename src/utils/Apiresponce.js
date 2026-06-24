d3dc6ed3e39879fa0fc87c7c9f41568e6fa2aeb3
M-Tanzil <tanzilrazachas@gmail.com>
2026-06-25 00:00:37 +0530

    utils setup

---
+++ src/utils/Apiresponce.js
@@ -0,0 +1,11 @@
+class Apiresponce{
+    constructor(
+        statuscode,
+        data, message = "Success"
+    ){
+        this.statuscode=statuscode,
+        this.data=data,
+        this.message=message
+        this.success = statuscode
+    }
+}
