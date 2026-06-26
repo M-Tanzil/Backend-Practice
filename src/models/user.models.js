9b4880a29aff64d8da74618b5365a01464eba844
M-Tanzil <tanzilrazachas@gmail.com>
2026-06-26 23:57:50 +0530

    Minor change

--- src/models/user.models.js
+++ src/models/user.models.js
@@ -1,4 +1,6 @@
 import mongoose ,{Schema} from "mongoose";
+import jwt from"jsonwebtoken";
+import bcrypt from "bcrypt";
 const userSchema = new Schema({
     username:{
         type : String,
@@ -43,6 +45,9 @@ const userSchema = new Schema({
 
 },{timestamps:true})
 
-
+userSchema.pre("save", async function (next) {
+    this.password= bcrypt.hash(this.password,10)
+    next()
+})
 
 export const User = mongoose.model("User",userSchema)
