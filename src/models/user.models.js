a6049ffcba97b8242122b4a8ff57961e8c1b3c1d
M-Tanzil <tanzilrazachas@gmail.com>
2026-06-26 23:18:42 +0530

    user model created

---
+++ src/models/user.models.js
@@ -0,0 +1,48 @@
+import mongoose ,{Schema} from "mongoose";
+const userSchema = new Schema({
+    username:{
+        type : String,
+        required:true,
+        unique:true,
+        index:true,
+        lowercase:true,
+        trim:true
+    },
+        email:{
+        type : String,
+        required:true,
+        unique:true,
+        lowercase:true,
+        trim:true
+    },
+        fullName:{
+        type : String,
+        index:true,
+        required:true,
+        trim:true
+    },
+        avatar:{
+            type:String,
+            required:true
+    },
+        coverimage:{
+            type:String,
+
+        },
+        watchHistory:[{
+            type:Schema.Types.ObjectId,
+            ref:"Video"
+        }],
+        password:{
+            type:String,
+            required:[true,"Password is required"]
+        },
+        refreshToken:{
+            type:String  
+        }
+
+},{timestamps:true})
+
+
+
+export const User = mongoose.model("User",userSchema)
